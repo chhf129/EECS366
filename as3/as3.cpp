1b551b5ab26787de1cf1dfb65a965d2fb96ac023
Haifeng <chhf129@gmail.com>
2015-09-20 02:41:16 -0400

    add basic geometric function from textbook

--- as3/as3.cpp
+++ as3/as3.cpp
@@ -6,7 +6,9 @@
 #include <stdlib.h>
 #include <GL\glut.h>
 #include <math.h>
+#include <iostream>
 
+typedef float Matrix4x4[4][4];
 #define ON 1
 #define OFF 0
 
@@ -23,8 +25,10 @@ float rho = 10;
 float eyeX, eyeY, eyeZ;
 int oldX, oldY;
 int rotate, zoom;
+Matrix4x4 currentMatrix;
 // Vertex and Face data structure sued in the mesh reader
 // Feel free to change them
+
 typedef struct _point {
 	float x, y, z;
 } point;
@@ -361,11 +365,132 @@ void	keyboard(unsigned char key, int x, int y)
 	glutPostRedisplay();
 }
 
+// These geometric functions are modified from chapter9-4
 
-// Here's the main
+/* Construct the 4 x 4 identity matrix. */
+
+void setIdentity(Matrix4x4 matIdent4x4)
+{
+	int row, col;
+
+	for (row = 0; row < 4; row++)
+		for (col = 0; col < 4; col++)
+			matIdent4x4[row][col] = (row == col);
+}
+
+/* Premultiply matrix m1 by matrix m2, store result in m2. */
+
+void matrixMultiply(Matrix4x4 m1, Matrix4x4 m2)
+{
+	int row, col;
+	Matrix4x4 matTemp;
+
+	for (row = 0; row < 4; row++)
+		for (col = 0; col < 4; col++)
+			matTemp[row][col] = m1[row][0] * m2[0][col] + m1[row][1] *
+			m2[1][col] + m1[row][2] * m2[2][col] +
+			m1[row][3] * m2[3][col];
+	for (row = 0; row < 4; row++)
+		for (col = 0; col < 4; col++)
+			m2[row][col] = matTemp[row][col];
+}
+
+/*  Procedure for generating 3D translation matrix.  */
+
+void matrixTranslate(float tx, float ty, float tz)
+{
+	Matrix4x4 matTransl3D;
+
+	//  Initialize translation matrix to identity.  
+	setIdentity(matTransl3D);
+
+	matTransl3D[0][3] = tx;
+	matTransl3D[1][3] = ty;
+	matTransl3D[2][3] = tz;
+
+	//  Concatenate matTransl3D with composite matrix.  
+	matrixMultiply(matTransl3D, currentMatrix);
+}
+
+/*  Procedure for generating a quaternion rotation matrix.  */
+
+void matrixRotate(_point p1, _point p2, float radianAngle)
+{
+	Matrix4x4 matQuatRot;
+
+	float axisVectLength = sqrt((p2.x - p1.x) * (p2.x - p1.x) +
+		(p2.y - p1.y) * (p2.y - p1.y) +
+		(p2.z - p1.z) * (p2.z - p1.z));
+	float cosA = cosf(radianAngle);
+	float oneC = 1 - cosA;
+	float sinA = sinf(radianAngle);
+	float ux = (p2.x - p1.x) / axisVectLength;
+	float uy = (p2.y - p1.y) / axisVectLength;
+	float uz = (p2.z - p1.z) / axisVectLength;
+
+	//  Set up translation matrix for moving p1 to origin,
+	// and concatenate translation matrix with currentMatrix.
+
+	matrixTranslate(-p1.x, -p1.y, -p1.z);
+
+	//  Initialize matQuatRot to identity matrix.  
+	setIdentity(matQuatRot);
+
+	matQuatRot[0][0] = ux*ux*oneC + cosA;
+	matQuatRot[0][1] = ux*uy*oneC - uz*sinA;
+	matQuatRot[0][2] = ux*uz*oneC + uy*sinA;
+	matQuatRot[1][0] = uy*ux*oneC + uz*sinA;
+	matQuatRot[1][1] = uy*uy*oneC + cosA;
+	matQuatRot[1][2] = uy*uz*oneC - ux*sinA;
+	matQuatRot[2][0] = uz*ux*oneC - uy*sinA;
+	matQuatRot[2][1] = uz*uy*oneC + ux*sinA;
+	matQuatRot[2][2] = uz*uz*oneC + cosA;
+
+	//  Concatenate matQuatRot with composite matrix.  
+	matrixMultiply(matQuatRot, currentMatrix);
+
+	// Construct inverse translation matrix for p1 and
+	//  concatenate with composite matrix.
+
+	matrixTranslate(p1.x, p1.y, p1.z);
+}
+
+/*  Procedure for generating a 3D scaling matrix.  */
+
+void matrixScale(float sx, float sy, float sz, _point fixedPt)
+{
+	Matrix4x4 matScale3D;
+
+	// Initialize scaling matrix to identity.  
+	setIdentity(matScale3D);
+
+	matScale3D[0][0] = sx;
+	matScale3D[0][3] = (1 - sx) * fixedPt.x;
+	matScale3D[1][1] = sy;
+	matScale3D[1][3] = (1 - sy) * fixedPt.y;
+	matScale3D[2][2] = sz;
+	matScale3D[2][3] = (1 - sz) * fixedPt.z;
+
+	//  Concatenate matScale3D with composite matrix.  
+	matrixMultiply(matScale3D, currentMatrix);
+}
+
+void printCurrentMatrix() {
+	std::cout << "current matrix\n";
+	std::cout << currentMatrix[0][0] << " " << currentMatrix[0][1] << " " << currentMatrix[0][2] << " " << currentMatrix[0][3] << '\n';
+	std::cout << currentMatrix[1][0] << " " << currentMatrix[1][1] << " " << currentMatrix[1][2] << " " << currentMatrix[1][3] << '\n';
+	std::cout << currentMatrix[2][0] << " " << currentMatrix[2][1] << " " << currentMatrix[2][2] << " " << currentMatrix[2][3] << '\n';
+	std::cout << currentMatrix[3][0] << " " << currentMatrix[3][1] << " " << currentMatrix[3][2] << " " << currentMatrix[3][3] << '\n';
+}
+
+
+
+
+
+
+
 int main(int argc, char* argv[])
 {
-	// Initialize GLUT
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 	glutCreateWindow("Assignment 2 Template (orthogonal)");
@@ -375,15 +500,11 @@ int main(int argc, char* argv[])
 	glutMotionFunc(OnMouseMove);
 	glutKeyboardFunc(keyboard);
 
-	// Initialize GL
-	//	glMatrixMode(GL_PROJECTION);
-	//glLoadIdentity();
-	//	glOrtho(-2.5, 2.5, -2.5, 2.5, -10000, 10000);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glEnable(GL_DEPTH_TEST);
 	meshReader("sphere.obj", 1);
-	// Switch to main loop
+	printCurrentMatrix();
 	glutMainLoop();
 	return 0;
 }
